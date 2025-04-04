404af2e9e060dd134a40a5651534e051f6dbcf1e
markPLP <mpclaspinas@gmail.com>
2025-04-04 20:46:59 +0800

    first commit

---
+++ js/navCollapse.js
@@ -0,0 +1,49 @@
+window.addEventListener('DOMContentLoaded', function () {
+  const navToggle = document.querySelector('.nav-toggle');
+  const navLinks = document.querySelector('.links');
+
+  navToggle.addEventListener('click', function () {
+    const isHiddenNav = navLinks.style.display === 'none';
+
+    if (isHiddenNav) {
+      expandNav(navLinks);
+    } else {
+      collapseNav(navLinks);
+    }
+  });
+
+  function expandNav(element) {
+    element.style.display = 'block';
+    element.style.height = '0';
+    element.style.height = `${element.scrollHeight}px`;
+    // use scrollHeight property to get actual height even if hidden or set to ZERO
+    //const getscrollHeight = `${element.scrollHeight}px`
+  }
+
+  function collapseNav(element) {
+    // const collapseHeight = element.offsetHeigt;
+    // element.style.height = `${collapseHeight}px`
+    element.style.transition = 'height 0.3s';
+    element.style.height = `${element.scrollHeight}px`;
+    element.style.height = 0;
+
+    setTimeout(() => {
+      element.style.display = 'none';
+      element.style.height = '';
+    }, 300);
+  }
+
+  function windowResize() {
+    navLinks.classList.add('mobile-nav');
+    if (document.documentElement.clientWidth < 800) {
+      navLinks.style.display = 'none';
+    } else {
+      navLinks.style.display = 'flex';
+      navLinks.style.height = 'auto';
+      navLinks.classList.remove('mobile-nav');
+    }
+  }
+
+  windowResize();
+  window.addEventListener('resize', windowResize);
+});
